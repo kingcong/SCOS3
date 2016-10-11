c663124121fda2b2dc6ded6dd78e987871297f1d
王聪 <2441413514@qq.com>
2016-10-11 16:13:15 +0800

    创建FoodView

---
+++ Android/SCOS3/app/src/main/java/activity/code/source/es/scos/es/source/code/activity/FoodView.java
@@ -0,0 +1,15 @@
+package activity.code.source.es.scos.es.source.code.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import activity.code.source.es.scos.R;
+
+public class FoodView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.food_view);
+    }
+}
--- Android/SCOS3/app/src/main/java/activity/code/source/es/scos/es/source/code/model/User.java
+++ Android/SCOS3/app/src/main/java/activity/code/source/es/scos/es/source/code/model/User.java
@@ -1,8 +1,40 @@
 package activity.code.source.es.scos.es.source.code.model;
 
+import java.io.Serializable;
+
 /**
  * Created by kingcong on 2016/10/11.
  */
 
-public class User {
+public class User implements Serializable{
+
+
+    private String userName;    // 用户名
+    private String password;    // 密码
+    private boolean oldUser;    // 是否是老用户
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isOldUser() {
+        return oldUser;
+    }
+
+    public void setOldUser(boolean oldUser) {
+        this.oldUser = oldUser;
+    }
+
 }
